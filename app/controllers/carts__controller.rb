608f838d95f7ef31521d3a6ccf80b7aa0f1e6793
om-nishimoto-ryoga <nishimoto.ryoga.infratop@gmail.com>
2019-06-29 18:03:21 +0900

    cart create

---
+++ app/controllers/carts__controller.rb
@@ -0,0 +1,22 @@
+class CartsController < ApplicationController
+  def index
+  	
+  	@carts = Cart.where(user_id: current_user.id)
+  end
+
+  def create
+
+	  @product = Product.find(params[:product_id])
+    @cart = current_user.carts.new(product_id: @product.id)
+    @cart.save
+    redirect_to carts_path
+
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
